3cdb896d8057e45b24b8f5152b034143b51f63c0
Błażej Rybarkiewicz <b.rybarkiewicz@gmail.com>
2020-06-17 22:46:34 +0200

    Initial commit

---
+++ app/src/main/java/com/github/abryb/bsm/MainActivity.java
@@ -0,0 +1,27 @@
+package com.github.abryb.bsm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void onCreatePassword(View view)
+    {
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.createPassword);
+        String message = editText.getText().toString();
+//        intent.putExtra("test", message);
+//        startActivity(intent);
+    }
+}
